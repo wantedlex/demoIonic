e9c7686d35365d70859cb61ad51e49c6ef0d4fb8
wantedlex <ale.corrado91@gmail.com>
2018-04-18 17:14:37 +0200

    Realizzato il refresh nella pagina dei contatti

--- www/controller/contactsController.js
+++ www/controller/contactsController.js
@@ -17,16 +17,25 @@ contactModule.config(function($stateProvider){
 
 
 // Controller per la view dei contatti
-contactModule.controller('ContactsController', function($log, RandomUserService){
+contactModule.controller('ContactsController', function($scope, $log, RandomUserService){
+    //Variabili interne al controller
     var _logPrefix = '[CONTROLLER CONTACTS]';
     var vm = this;
+
+    vm.refreshContactList = function(){
+        vm.init();
+        $scope.$broadcast('scroll.refreshComplete');        // Questo mi serve per informare il componente ion-refresher che è stata completata l'operazione.
+    };                                                      // Dall'altro lato, nel controller, ci sarà un watcher, listner o simile che sarà in ascolto di questo evento.
     
+    //Variabili del controller esposte alla view
     vm.listOfContact = [];
+    vm.loading = true;                      //Mi serve per togliere lo spinner quando ha finito il caricamento
 
     vm.init = function(){
         RandomUserService.getPeople(50,'fr,nl,us,es')
             .then(function(people){
                 vm.listOfContact = people.data.results;                       // Assegno alla lista dei contatti tutti gli oggetti che vado a ricavarmi
+                vm.loading = false;
                 $log.debug(_logPrefix + 'ok', this.listOfContact);
             })
             .catch(function(){
